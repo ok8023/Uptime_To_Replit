d294425fa004978212211af9e7e04f69007eec69
ok8023 <130885038+ok8023@users.noreply.github.com>
2023-04-16 22:09:10 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,4 +4,5 @@ echo "开源地址：https://github.com/ok8023/Uptime_To_Replit/"
 git clone https://github.com/ok8023/Uptime_To_Replit && mv -b Uptime_To_Replit/install/* ./ && mv -b Uptime_To_Replit/install/.[^.]* ./ && rm -rf *~ && rm -rf Uptime_To_Replit
 echo "恭喜项目搭建完成"
 echo "点击Run立即运行项目"
-echo "查询用户名及密码运行下面命令"
+echo "成功部署后注释掉main.sh第一行的 git clone"
+echo "示例：#git clone https://github.com/louislam/uptime-kuma.git"
