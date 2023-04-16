fc1d9da8c5aaf1c279402b3b5c224cca257d2d9b
ok8023 <130885038+ok8023@users.noreply.github.com>
2023-04-16 20:51:44 +0800

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,7 @@
+echo "Uptime_To_Replit 一键搭建脚本"
+echo "脚本作者：ok8023"
+echo "开源地址：https://github.com/ok8023/Uptime_To_Replit/"
+git clone https://github.com/ok8023/Uptime_To_Replit && mv -b Uptime_To_Replit/install/* ./ && mv -b Uptime_To_Replit/install/.[^.]* ./ && rm -rf *~ && rm -rf Uptime_To_Replit
+echo "恭喜项目搭建完成"
+echo "点击Run立即运行项目"
+echo "查询用户名及密码运行下面命令"
